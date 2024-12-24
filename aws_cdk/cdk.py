2990cad09d35e609cdce56b7669eb1be891517bb
CoffeeIsNak <tjdqnzo@naver.com>
2024-12-25 02:09:40 +0900

    chore: linting done

--- aws_cdk/cdk.py
+++ aws_cdk/cdk.py
@@ -4,6 +4,7 @@ from aws_cdk import (
     core
 )
 
+
 class EKSClusterStack(core.Stack):
     def __init__(self, scope: core.Construct, id: str, **kwargs) -> None:
         super().__init__(scope, id, **kwargs)
@@ -12,11 +13,13 @@ class EKSClusterStack(core.Stack):
         vpc = ec2.Vpc(self, "MyVPC", max_azs=2)
 
         # EKS 클러스터 생성
-        cluster = eks.Cluster(self, "MyEKSCluster",
-            vpc=vpc,
-            default_capacity=2,  # 기본 노드 수
-            default_capacity_instance=ec2.InstanceType("t3.medium")  # EC2 인스턴스 유형
-        )
+        cluster = eks.Cluster(self,
+                              "MyEKSCluster",
+                              vpc=vpc,
+                              default_capacity=2,
+                              default_capacity_instance=ec2.InstanceType(
+                                  "t3.medium")
+                              )
 
         # EKS 클러스터의 kubeconfig 파일 업데이트
         cluster.add_auto_scaling_group_capacity(
@@ -27,6 +30,7 @@ class EKSClusterStack(core.Stack):
         # 클러스터 정보 출력 (optional)
         core.CfnOutput(self, "ClusterName", value=cluster.cluster_name)
         core.CfnOutput(self, "ClusterEndpoint", value=cluster.cluster_endpoint)
+
 
 app = core.App()
 EKSClusterStack(app, "EKSClusterStack")
